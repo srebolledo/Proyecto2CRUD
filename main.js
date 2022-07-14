66c5b41b11f4a7592ae35154fb849d08c31dfe5f
RomiFerCh <romi.fer@icloud.com>
2022-07-14 17:46:24 -0400

    x

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
-function recibirNombre(Nombre, Apellido) {
-    
+//definición de variables
+const contenedor = document.querySelector('tbody')
+const formArticulo = document.querySelector('#form')
+const descripcion = document.getElementById('descripcion')
+const precio =document.getElementById('precio')
+const stock =document.getElementById('stock')
+const categoria =document.getElementById('categoria')
+let arrayArticuloNuevo = []
+
+   //Funciones
+
+btnCrear.addEventListener('click', ()=>{
+ })
+
+const crearItem = (productoN) => {
+    let item = {
+        descripcion: productoN,
+        estado: true
+
+    }
 }
+
+const guardar = () => {
+    localStorage.setItem('producto', JSON.stringify(arrayArticuloNuevo))
+}
+const verForm = () => {
+    descripcion.innerHTML = '';
+
+}
+
+//addEvent
+
+formArticulo.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let agregarProducto = document.querySelector('descripcion').value;
+
+    crearItem(descripcion)
+    guardar()
+
+    formularioUI.reset()  
+
+
+  
+})
+document.addEventListener('DOMContentLoaded', verForm)
