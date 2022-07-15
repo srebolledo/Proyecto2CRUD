8dec6153be31106e009fc8308fe5789b01b2c165
RomiFerCh <romi.fer@icloud.com>
2022-07-15 12:46:59 -0400

    llenado de tabla

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
-//definición de variables
-const contenedor = document.querySelector('tbody')
-const formArticulo = document.querySelector('#form')
-const descripcion = document.getElementById('descripcion')
-const precio =document.getElementById('precio')
-const stock =document.getElementById('stock')
-const categoria =document.getElementById('categoria')
-let arrayArticuloNuevo = []
 
-   //Funciones
+let arrayArticulos = []
+let tabla1 = document.getElementById('tabla1')
 
-btnCrear.addEventListener('click', ()=>{
- })
+function llenarTabla(){
+    tabla1.innerHTML=''
 
-const crearItem = (productoN) => {
-    let item = {
-        descripcion: productoN,
-        estado: true
+    arrayArticulos.forEach((producto, index) => {
+        tabla1.innerHTML+= `
+        <tr>
+            <td>${index +1}</td>
+            <td>${producto.descripcion}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.stock}</td>
+            <td>
+                <span class="mx-3 float-end">
+                    <i class="fa-solid editar fa-pen-to-square mx-2"></i>
+                    <i class="fa-solid trash fa-trash-can mx-2"></i>
+                </span>
+                </td>
+        </tr>
+        `
+
+    })
+    accionBtn()
+
+}
+function accionBtn(){
+    let btnEditar = Array.from(document.getElementsByClassName('editar'))
+    let btnTrash = Array.from(document.getElementsByClassName('trash'))
+    
+console.log(btnEditar);
+//falta los aggregar eventListener
+}
+const guardar = () => {
+     localStorage.setItem('producto', JSON.stringify(arrayArticulos))
+ }
+ const verForm = () => {
+     descripcion.innerHTML = '';
+ }
+
+
+ 
+llenarTabla()
+
+let nombre = document.getElementById('descripcion')
+let price = document.getElementById('precio')
+let cantidad = document.getElementById('stock')
+let boton = document.getElementById('btnCrear')
+
+boton.addEventListener('click', agregarProducto)
+
+function agregarProducto(){
+    let producto = {
+        descripcion: nombre.value,
+        precio: price.value,
+        stock: cantidad.value,
 
     }
+    arrayArticulos.push(producto)
+    llenarTabla()
+    
 }
-
-const guardar = () => {
-    localStorage.setItem('producto', JSON.stringify(arrayArticuloNuevo))
-}
-const verForm = () => {
-    descripcion.innerHTML = '';
-
-}
-
-//addEvent
-
-formArticulo.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let agregarProducto = document.querySelector('descripcion').value;
-
-    crearItem(descripcion)
-    guardar()
-
-    formularioUI.reset()  
-
-
-  
-})
-document.addEventListener('DOMContentLoaded', verForm)
